ae72fec1999eea3678ec82eb5a3d09f54f1714fd
Amber Abreu <ambermabreu@gmail.com>
2022-02-23 16:34:30 -0500

    updated port

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
