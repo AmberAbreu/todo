4db18a96195f019536ee1246fd9b647a33c76a8d
Amber Abreu <ambermabreu@gmail.com>
2022-02-25 23:58:23 -0500

    Heroku

--- server/index.js
+++ server/index.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const path = require("path");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
-
-
 
 const todos = require('./api/todos')
 
-app.use(express.static(path.resolve(__dirname, "../client/public")));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+
 
 app.use("/todos", todos);
 
 app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "..", "client/public", "index.html"));
+	res.sendFile(path.resolve(__dirname, "..", "client/build", "index.html"));
 });
 
 app.listen(port, () => {
